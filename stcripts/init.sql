69a13e98710e5bfd4724466247ecf6118cc2760f
Chekryzhov Arkadiy <chekryzhov2000@mail.ru>
2020-11-01 00:24:31 +0300

    sql

--- stcripts/init.sql
+++ stcripts/init.sql
@@ -1,3 +1,37 @@
+create database kino_park;
+
+use kino_park
+
+create table users
+(
+    id int auto_increment primary key,
+    username varchar(80) not null,
+    password varchar(80) not null,
+    email    varchar(80) not null,
+    image    varchar(80)
+);
+
+create table session
+(
+    id       varchar(80) primary key,
+    username varchar(80) not null
+);
+
+create table films
+(
+    id          int auto_increment primary key,
+    title       varchar(80) character set 'utf8' not null,
+    description varchar(255) character set 'utf8' not null,
+    rating      int DEFAULT 0,
+    sumVotes      int DEFAULT 0,
+    mainGenre   varchar(80) character set 'utf8' not null,
+    youtubeLink varchar(255) character set 'utf8' not null,
+    bigImg      varchar(255) not null,
+    smallImg    varchar(255) not null,
+    year        int not null,
+    country     varchar(80) character set 'utf8'
+);
+
 INSERT INTO films(id, title, description, mainGenre, youtubeLink, bigImg, smallImg, year, country)
 VALUES
     (1, 'Начало', 'Шпионаж фантастического уровня. С помощью сверхтехнологии герой Ди Каприо и его команда проникают в чужие сны', 'Фантастика', 'https://www.youtube.com/embed/85Zz1CCXyDI', 'http://fullhdwallpapers.ru/image/movies/24609/film-nachalo-inception.jpg', 'https://st.kp.yandex.net/im/poster/1/3/1/kinopoisk.ru-Inception-1310268.jpg', 2010, 'США/Великобритания'),
@@ -56,3 +90,23 @@ VALUES
         1998,
         'США'
     );
+
+create table rating
+(
+    id int auto_increment primary key,
+    rating int,
+    film_id int,
+    user_id int
+);
+
+CREATE TRIGGER trigger1
+AFTER INSERT
+ON rating
+FOR EACH ROW
+update films set rating = ((rating * sumVotes + new.rating) / (sumVotes + 1)), sumVotes = sumVotes + 1 where id = new.film_id;
+
+CREATE TRIGGER trigger2
+BEFORE UPDATE
+ON rating
+FOR EACH ROW
+update films set rating = ((rating * sumVotes - old.rating + new.rating) / sumVotes) where id = new.film_id;
