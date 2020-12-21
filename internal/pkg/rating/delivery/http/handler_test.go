eb385b16f83659cf5833843485403bea1de9bbe1
ErikNabiev <er2000nabiev@mail.ru>
2020-12-22 01:09:30 +0300

    tests

--- internal/pkg/rating/delivery/http/handler_test.go
+++ internal/pkg/rating/delivery/http/handler_test.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"errors"
 	"github.com/Arkadiyche/http-rest-api/internal/pkg/models"
 	"github.com/Arkadiyche/http-rest-api/internal/pkg/rating"
 	"github.com/golang/mock/gomock"
@@ -54,5 +55,37 @@ func TestGetReviews(t *testing.T) {
 		handler := http.HandlerFunc(ratingHandler.ShowReviews)
 		handler.ServeHTTP(rr, req)
 		assert.Equal(t, rr.Body.String(), "[{\"id\":5,\"user_login\":\"arkadiy\",\"text_body\":\"aaaa\",\"user_id\":1,\"film_id\":10,\"rate\":1}]")
+	})
+
+	t.Run("FindById-fail", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		m := rating.NewMockUseCase(ctrl)
+
+		m.
+			EXPECT().
+			GetReviews(gomock.Eq("10")).
+			Return(&testReviews, errors.New("error"))
+		ratingHandler := RatingHandler{
+			UseCase: m,
+			Logger:  logrus.New(),
+		}
+		req, err := http.NewRequest("GET", "/person_film/10", nil)
+
+		vars := map[string]string{
+			"film_id": "10",
+		}
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req = mux.SetURLVars(req, vars)
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(ratingHandler.ShowReviews)
+		handler.ServeHTTP(rr, req)
+		assert.Equal(t, rr.Code, 400)
 	})
 }
