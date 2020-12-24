9d71fd89ee0c0476566ac51aace67266bc50dd26
Chekryzhov Arkadiy <chekryzhov2000@mail.ru>
2020-12-24 16:33:52 +0300

    micro

--- internal/pkg/microsevice/rate/cmd/main.go
+++ internal/pkg/microsevice/rate/cmd/main.go
@@ -6,7 +6,6 @@ import (
 	rate "github.com/Arkadiyche/http-rest-api/internal/pkg/microsevice/rate/server"
 	"github.com/Arkadiyche/http-rest-api/internal/pkg/models"
 	"github.com/Arkadiyche/http-rest-api/internal/pkg/store"
-	"github.com/BurntSushi/toml"
 	"log"
 )
 
@@ -14,22 +13,17 @@ import (
 func main() {
 
 	config := apiserver.NewConfig()
-	_, err := toml.DecodeFile("/home/ubuntu/back/2020_2_Ruiners/configs/auth.toml", config)
-	if err != nil {
-		config = &apiserver.Config{
-			BindAddr: ":8002",
-			LogLevel: "debug",
-			Store:    &store.Config{DatabaseURL: "root:password@/kino_park"},
-			CORS:     models.CORSConfig{},
-		}
+	config = &apiserver.Config{
+		BindAddr: ":8003",
+		LogLevel: "debug",
+		Store:    &store.Config{DatabaseURL: "root:password@/kino_park"},
+		CORS:     models.CORSConfig{},
 	}
 	db := store.New(config.Store)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+
 	fmt.Println(db.Config())
 	srv := rate.NewServer(config.BindAddr, db)
-	if err = srv.ListenAndServe(); err != nil {
+	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err.Error())
 	}
 }
--- internal/pkg/microsevice/session/cmd/main.go
+++ internal/pkg/microsevice/session/cmd/main.go
@@ -6,29 +6,22 @@ import (
 	session "github.com/Arkadiyche/http-rest-api/internal/pkg/microsevice/session/server"
 	"github.com/Arkadiyche/http-rest-api/internal/pkg/models"
 	"github.com/Arkadiyche/http-rest-api/internal/pkg/store"
-	"github.com/BurntSushi/toml"
 	"log"
 )
 
 func main() {
 
 	config := apiserver.NewConfig()
-	_, err := toml.DecodeFile("/home/ubuntu/back/2020_2_Ruiners/session.toml", config)
-	if err != nil {
-		config = &apiserver.Config{
-			BindAddr: ":8003",
-			LogLevel: "debug",
-			Store:    &store.Config{DatabaseURL: "root:password@/kino_park"},
-			CORS:     models.CORSConfig{},
-		}
+	config = &apiserver.Config{
+		BindAddr: ":8002",
+		LogLevel: "debug",
+		Store:    &store.Config{DatabaseURL: "root:password@/kino_park"},
+		CORS:     models.CORSConfig{},
 	}
 	db := store.New(config.Store)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
 	fmt.Println(db.Config())
 	srv := session.NewServer(config.BindAddr, db)
-	if err = srv.ListenAndServe(); err != nil {
+	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err.Error())
 	}
 }
