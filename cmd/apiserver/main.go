953da09e7e82bc9341312813837c7336cf1b3851
Chekryzhov Arkadiy <chekryzhov2000@mail.ru>
2020-12-24 16:01:20 +0300

    tryX100000

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"flag"
 	"github.com/Arkadiyche/http-rest-api/internal/app/apiserver"
-	"github.com/Arkadiyche/http-rest-api/internal/pkg/models"
-	"github.com/Arkadiyche/http-rest-api/internal/pkg/store"
+	"github.com/BurntSushi/toml"
 	"log"
 )
 
+var (
+	configPath string
+)
 
+func init() {
+	flag.StringVar(&configPath, "config-path", "/home/ubuntu/back/2020_2_Ruiners/bin/apiserve", "path to config file")
+}
 
 func main() {
+	flag.Parse()
 
-	config := apiserver.Config{
-		BindAddr: ":8000",
-		LogLevel: "debug",
-		Store:    &store.Config{DatabaseURL: "root:password@/kino_park"},
-		CORS:     models.CORSConfig{},
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
 	}
-	s := apiserver.New(&config)
+	s := apiserver.New(config)
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
 }
+
+
+//config := apiserver.Config{
+//BindAddr: ":8000",
+//LogLevel: "debug",
+//Store:    &store.Config{DatabaseURL: "root:password@/kino_park"},
+//CORS:     models.CORSConfig{},
+//}
