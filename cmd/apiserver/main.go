9cf4dbfc0a2209997f745eca105bfcd5e654777f
Chekryzhov Arkadiy <chekryzhov2000@mail.ru>
2020-12-24 15:39:50 +0300

    tryX100000

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,29 +1,23 @@
 package main
 
 import (
-	"flag"
 	"github.com/Arkadiyche/http-rest-api/internal/app/apiserver"
-	"github.com/BurntSushi/toml"
+	"github.com/Arkadiyche/http-rest-api/internal/pkg/models"
+	"github.com/Arkadiyche/http-rest-api/internal/pkg/store"
 	"log"
 )
 
-var (
-	configPath string
-)
 
-func init() {
-	flag.StringVar(&configPath, "config-path", "../configs/apiserver.toml", "path to config file")
-}
 
 func main() {
-	flag.Parse()
 
-	config := apiserver.NewConfig()
-	_, err := toml.DecodeFile(configPath, config)
-	if err != nil {
-		log.Fatal(err)
+	config := apiserver.Config{
+		BindAddr: ":8000",
+		LogLevel: "debug",
+		Store:    &store.Config{DatabaseURL: "root:password@/kino_park"},
+		CORS:     models.CORSConfig{},
 	}
-	s := apiserver.New(config)
+	s := apiserver.New(&config)
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
